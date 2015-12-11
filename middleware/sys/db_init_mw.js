41ca521257ef02865177b4d29a7c136ddaf13aad
pigo <zhenglin.zhu@xfxb.net>
2015-12-11 09:51:33 +0800

    make init sql by read file instead of sql statement of String model

--- middleware/sys/db_init_mw.js
+++ middleware/sys/db_init_mw.js
@@ -6,17 +6,33 @@
  * @version: v0.0.1
  */
 "use strict";
+var path = require('path');
+var fs = require('fs');
 var sql = require('../../sql');
 var pool = require('../../dao/base_dao').pool;
 
 function DBInitMiddleware(){
 
 }
+/**
+ * private
+ * @constructor
+ */
+function loadSQL(){
+    let filePath = path.join(__dirname,'../../sql/tiramisu.sql');
+    var sql = fs.readFileSync(filePath,{
+        flag : 'rs',
+        encoding : 'utf-8'
+    });
+    return sql;
+
+}
+
 
 DBInitMiddleware.initdb = ()=>{
     if(process.env.tiramisu_env !== 'production'){
         pool.getConnection((err,conn)=> {
-            conn.query(sql, (err)=> {
+            conn.query(loadSQL(), (err)=> {
                 if (err) {
                     console.log(err);
                     console.log('=============================================');

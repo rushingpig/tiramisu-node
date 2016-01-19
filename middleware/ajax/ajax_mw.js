6480645884ee91505c99189563c75e5e8a12e0b1
pigo <zhenglin.zhu@xfxb.net>
2016-01-19 16:04:16 +0800

    add fields about orders...

--- middleware/ajax/ajax_mw.js
+++ middleware/ajax/ajax_mw.js
@@ -16,7 +16,7 @@ function AjaxMw(){
  * @param next
  */
 AjaxMw.allowJsonp = function(req,res,next){
-    res.header("Access-Control-Allow-Origin", config.common.cors.Origin);
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, sessionid, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
     next();

b86b79a2e4353d67a9d7b0f9dc2a5802f92bfcae
Deo <liangdeo@Deos-MacBook-Pro.local>
2016-02-02 08:35:45 +0800

    direct copy from add_order and cosmetic change

---
+++ schema/buss/order/add_external_order.js
@@ -0,0 +1,53 @@
+'use strict';
+module.exports = {
+  'delivery_type': {
+    notEmpty: true
+  },
+  'owner_name': {
+    notEmpty: true
+  },
+  'owner_mobile': {
+    isMobilePhone: {
+      options: ['zh-CN']
+    }
+  },
+  'recipient_name': {
+    notEmpty: true
+  },
+  'recipient_mobile': {
+    isMobilePhone: {
+      options: ['zh-CN']
+    }
+  },
+  'regionalism_id': {
+    notEmpty: true,
+    isInt: true
+  },
+  'recipient_address': {
+    notEmpty: true
+  },
+  //'delivery_id': {
+  //notEmpty: true,
+  //isInt : true
+  //},
+  'src_id': {
+    notEmpty: true,
+    isInt: true
+  },
+  'pay_modes_id': {
+    notEmpty: true,
+    isInt: true
+  },
+  'pay_status': {
+    notEmpty: true
+  },
+  'delivery_time': {
+    notEmpty: true
+  },
+  'total_amount': {
+    notEmpty: true
+  },
+  'greeting_card': {
+    optional: true
+  }
+};

b05995013ce301cfb9030135eff959868d4b8efb
Deo <zhanzhao.liang@sharinspace.com>
2016-02-03 15:36:40 +0800

    add deployment shell script

---
+++ deploy_tiramisu.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# created-by : pigo.can
+# created-at : 2015-12-11
+# desc : to deploy the project -> 'tiramisu'
+
+project_name="tiramisu"
+project_path="/project/tiramisu"
+git=`which git`
+pm2=`which pm2`
+npm=`which npm`
+
+function init(){
+	cd "${project_path}"
+	${git} pull
+	${npm} install
+}
+
+function start(){
+	${pm2} start "${project_name}"
+}
+
+function stop(){
+	${pm2} stop "${project_name}"
+}
+
+function restart(){
+	${pm2} restart "${project_name}"
+}
+function reload(){
+	${pm2} reload "${project_name}"
+}
+
+function usage(){
+	echo "Usage:::::::::: sh|./ $0 { start | restart | stop | reload }"
+	exit 1
+}
+
+case "$1" in
+	start | restart | stop | reload)
+		init
+		$1
+	;;
+	*)
+		usage
+	;;
+esac

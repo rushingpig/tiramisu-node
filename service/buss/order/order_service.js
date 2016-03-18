0d282d188116ee5b45b0c59d698ad1898ac7982d
pigo <pigo@Pigo-MBP.local>
2016-03-18 20:09:37 +0800

    optimize the mysql pool connection release...

--- service/buss/order/order_service.js
+++ service/buss/order/order_service.js
@@ -168,6 +168,7 @@ OrderService.prototype.getOrderDetail = (req, res, next) => {
       data.recipient_landmark = curr.landmark;
       data.updated_time = curr.updated_time;
       data.status = curr.status;
+      data.merchant_id = curr.merchant_id;
       data.total_amount = curr.total_amount;  // 总应收金额
       data.total_discount_price = curr.total_discount_price;  // 总实际售价
       data.total_original_price = curr.total_original_price;  // 总原价

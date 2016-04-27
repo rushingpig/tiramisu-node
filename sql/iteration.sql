8ee3607d168dc4125b2ab84379ed3a7d0489dc27
Deo <zhanzhao.liang@sharinspace.com>
2016-04-27 09:22:06 +0800

    sql update

---
+++ sql/iteration.sql
@@ -0,0 +1,3 @@
+# 2016-04-27 Zhanzhao Liang
+INSERT INTO `dict_regionalism` VALUES
+   ('442027', '中山市', '442000', '中山', '3', '0760', '528400', '中国,广东省,中山市,中山市', '113.378835', '22.52522', 'Zhongshan', '1', null, null);

c6c4f4d61e5c5f678dea20ebb05900af53b4c1f2
Deo <zhanzhao.liang@sharinspace.com>
2016-04-27 15:53:15 +0800

    add 配件 category

--- sql/iteration.sql
+++ sql/iteration.sql
@@ -1,3 +1,4 @@
 # 2016-04-27 Zhanzhao Liang
 INSERT INTO `dict_regionalism` VALUES
    ('442027', '中山市', '442000', '中山', '3', '0760', '528400', '中国,广东省,中山市,中山市', '113.378835', '22.52522', 'Zhongshan', '1', null, null);
+INSERT INTO `buss_product_category` VALUES (15, 0, '配件', 0, 1, now(), null, null, 1);

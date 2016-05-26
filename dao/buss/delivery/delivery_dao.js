7854364a9fcde974d07dda206f208f7f32ecbf2c
zhaowei <wei.zhao@xfxb.net>
2016-05-26 16:12:25 +0800

    修复拼接地址不完整

--- dao/buss/delivery/delivery_dao.js
+++ dao/buss/delivery/delivery_dao.js
@@ -304,6 +304,8 @@ DeliveryDao.prototype.findDeliveryRecord = function (begin_time, end_time, city_
     params.push(tables.buss_delivery_record);
     sql += `LEFT JOIN ?? bpm ON bo.pay_modes_id = bpm.id `;
     params.push(tables.buss_pay_modes);
+    sql += `INNER JOIN ?? dr on dr.id = br.regionalism_id `;
+    params.push(tables.dict_regionalism);
     sql += `WHERE bo.status IN ('${constant.OS.COMPLETED}', '${constant.OS.EXCEPTION}') `;
     if(begin_time){
         sql += `AND bo.delivery_time >= ? `;

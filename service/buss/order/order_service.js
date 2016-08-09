5542e3b089783f8f74a74ae5a2e13025c8b51f52
Zhaowei <wei.zhao@xfxb.net>
2016-08-09 14:43:51 +0800

    fix bind_order_id

--- service/buss/order/order_service.js
+++ service/buss/order/order_service.js
@@ -732,8 +732,8 @@ OrderService.prototype.listOrders = (entrance, isBatchScan) => {
           signin_time: curr.signin_time,
           greeting_card: curr.greeting_card
         };
-        if (curr.bind_order_id != '0') {
-          list_obj.bind_order_id = yield orderDao.joinOrderId(list_obj.bind_order_id);
+        if (curr.bind_order_id && curr.bind_order_id != '0') {
+          list_obj.bind_order_id = yield orderDao.joinOrderId(curr.bind_order_id);
         }
         let refund_obj = yield refundDao.findLastRefundByOrderId(curr.id);
         if (refund_obj) {

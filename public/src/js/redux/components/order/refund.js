b0d753e65b1507fa93ccf5208a2ce66d0785014c
BerthaPeng <858005492@qq.com>
2016-08-12 15:21:14 +0800

    修正获取城市

--- public/src/js/redux/components/order/refund.js
+++ public/src/js/redux/components/order/refund.js
@@ -100,7 +100,7 @@ class FilterHeader extends Component{
 	}
 	onProvinceChange(callback, e){
 		var {value } = e.target;
-		this.props.getCitiesSignal(value, 1,  'authority');
+		this.props.getCitiesSignal({province_id: value, is_standard_area: 1, signal: 'authority'});
 		callback(e);
 	}
 	search(search_in_state){
